ea8458a39531dda29a94dcbed647813be4e670f1
Torio Yoshiko <torio.yoshiko@gmail.com>
2020-11-26 22:50:01 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# circleci-find-tag
+
+## The Problem
+
+CircleCI UI doesn't have an option to find a build by the git tag. 
+
+## Installation
+
+Install the package by using either
+
+```
+npm i --g circleci-find-tag
+```
+
+or
+
+```
+yarn global add circleci-find-tag
+```
+
+## Usage
+
+```
+circleci-find-tag <Repository Prefix> <CircleCi Token> <tag>
+```
+
+**Repository prefix** - part of the url uniquely identifying a project. 
+
+app.circleci.com/pipelines/**github/org/project**
+
+**CircleCI token** - CircleCI API access token. [Read here how to get one](https://circleci.com/docs/2.0/api-developers-guide/#add-an-api-token).
+
+**tag** - git tag to find a build associated to
+
+Example command:
+
+```
+circleci-find-tag github/org/project token release-1.0
+```
+
+Example output:
+
+```
+release-1.12 -> https://circleci.com/gh/org/project/3116
+```
---
+++ index.js
@@ -0,0 +1,64 @@
+#!/usr/bin/env node
+const https = require('https');
+
+const getBuildPage = async (url, token, limit, offset) => {
+    const options = {
+        hostname: 'circleci.com',
+        port: 443,
+        path: `/api/v1.1/project/${url}?circle-token=${token}&limit=${limit}&offset=${offset}`,
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+        }
+    }
+
+    const promise = new Promise((resolve, reject) => {
+
+        const request = https.request(options, response => {
+            let responseString = '';
+
+            response.on('data', d => {
+                responseString += (d.toString());
+            })
+
+            response.on('end', () => {
+                resolve(JSON.parse(responseString));
+            });
+        })
+
+        request.on('error', error => {
+            reject(error);
+        });
+
+        request.end()
+    });
+
+    return promise;
+
+}
+
+const getTags = async () => {
+    let args = process.argv;
+
+    let offset = 0;
+
+    while (true){
+        let results = await getBuildPage(args[2], args[3], 50, offset);
+
+        for (let i = 0; i < results.length; i++){
+            if (results[i].vcs_tag === args[4]){
+                console.log(args[4] + ' -> ' + results[i].build_url);
+                return;
+            }
+        }
+
+        if (results.length < 50){
+            console.log('Tag not found');
+            break;
+        }
+
+        offset += 50;
+    }
+}
+
+getTags();
