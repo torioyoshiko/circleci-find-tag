794abcb875e25de43152549459d22bfb68f330b9
torioyoshiko <torio.yoshiko@gmail.com>
2022-01-17 10:58:49 +0100

    Now tag is not required, added keywords and example without tag.

--- README.md
+++ README.md
@@ -23,7 +23,7 @@ yarn global add circleci-find-tag
 ## Usage
 
 ```
-circleci-find-tag <Repository Prefix> <CircleCi Token> <tag>
+circleci-find-tag <Repository Prefix> <CircleCi Token> [tag]
 ```
 
 **Repository prefix** - part of the url uniquely identifying a project. 
@@ -32,16 +32,31 @@ app.circleci.com/pipelines/**github/org/project**
 
 **CircleCI token** - CircleCI API access token. [Read here how to get one](https://circleci.com/docs/2.0/api-developers-guide/#add-an-api-token).
 
-**tag** - git tag to find a build associated to
+**tag** - *not required* - git tag to find a build associated with, if not provided, it will print out all tagged builds.
 
-Example command:
+Example command with tag:
 
 ```
 circleci-find-tag github/org/project token release-1.0
 ```
 
-Example output:
+Example output with tag:
 
 ```
 release-1.12 -> https://circleci.com/gh/org/project/3116
+```
+
+Example command without tag:
+
+```
+circleci-find-tag github/org/project token
+```
+
+Example output without tag:
+
+```
+release-1.12 -> https://circleci.com/gh/org/project/3116
+release-1.26 -> https://circleci.com/gh/org/project/3116
+release-2.03 -> https://circleci.com/gh/org/project/3116
+release-2.12 -> https://circleci.com/gh/org/project/3116
 ```
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+// #!/usr/bin/env node
 const https = require('https');
 
 const getBuildPage = async (url, token, limit, offset) => {
@@ -39,22 +39,45 @@ const getBuildPage = async (url, token, limit, offset) => {
 
 const getTags = async () => {
     let args = process.argv;
-
+    let url = args[2];
+    let token = args[3];
+    let tag = args[4]
     let offset = 0;
 
-    while (true){
-        let results = await getBuildPage(args[2], args[3], 50, offset);
+    if (args.length < 4) {
+        console.log(
+            'Invalid number of arguments.\n' +
+            'Usage: circleci-find-tag <Repository Prefix> <CircleCi Token> [tag]'
+        )
+        return;
+    }
 
-        let build = results.find(elem => elem.vcs_tag === args[4])
+    while (true) {
+        let results = await getBuildPage(url, token, 50, offset);
 
-        if (build !== undefined){
-            console.log(args[4] + ' -> ' + build.build_url);
-            return;
-        }
 
-        if (results.length < 50){
-            console.log('Tag not found');
-            break;
+        if (!tag) {
+            let builds = results.filter(elem => elem.vcs_tag)
+
+            for (let build of builds) {
+                console.log(build.vcs_tag + ' -> ' + build.build_url);
+            }
+
+            if (results.length < 50) {
+                break;
+            }
+        } else {
+            let build = results.find(elem => elem.vcs_tag === tag)
+
+            if (build !== undefined) {
+                console.log(tag + ' -> ' + build.build_url);
+                return;
+            }
+
+            if (results.length < 50) {
+                console.log('Tag not found');
+                break;
+            }
         }
 
         offset += 50;
